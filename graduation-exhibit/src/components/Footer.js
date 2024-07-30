295dec196acf64375af708564039cee1a258700f
Suhyeon7 <zxguszx@naver.com>
2024-07-30 22:27:10 +0900

    professor modify

---
+++ graduation-exhibit/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <hr/>
+      2024 SOFTWARE GRADUATION EXHIBITION
+      <br />
+      <b>Algorithm: Rhythm in Code</b>
+    </footer>
+  );
+}
+
+export default Footer;
